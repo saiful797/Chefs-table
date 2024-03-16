f807f35f4b325a68c81d7006672f681d0d082b96
saiful797 <saifulislamsaif797@gmail.com>
2024-03-16 22:17:00 +0600

    Added JSON file

--- src/components/Our-recipes/Ourrecipies.jsx
+++ src/components/Our-recipes/Ourrecipies.jsx
@@ -2,7 +2,7 @@ const Ourrecipies = () => {
     return (
         <div className="mt-10 text-center space-y-8">
             <h1 className="text-4xl font-bold ">Our Recipes</h1>
-            <p className="w-2/3 mx-auto">Recipes are windows into culture and creativity, offering a glimpse into culinary traditions and innovative flavors. Whether passed down through generations or crafted by renowned chefs, they inspire experimentation and shared meals, enriching our culinary experiences.</p>
+            <p className="w-2/3 mx-auto text-lg font-medium">Recipes are windows into culture and creativity, offering a glimpse into culinary traditions and innovative flavors. Whether passed down through generations or crafted by renowned chefs, they inspire experimentation and shared meals, enriching our culinary experiences.</p>
         </div>
     );
 };
