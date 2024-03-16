cf4cb50bdde7d88ad2925444970aa6c8b7122fbf
saiful797 <saifulislamsaif797@gmail.com>
2024-03-16 07:23:03 +0600

    Added navbar section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
+import Navbar from './components/navbar/Navbar'
 
 function App() {
   return (
     <>
-      <h1 className='text-5xl'>Recipe Calories</h1>
+      <Navbar></Navbar>
     </>
   )
 }
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,37 @@
+export default function Navbar(){
+    return(
+        <div className="navbar bg-base-10">
+            <div className="navbar-start">
+               <div className="dropdown">
+                    <div tabIndex="0" role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </div>
+                    <ul tabIndex="0" className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><a>Home</a></li>
+                        <li><a>Recipes</a></li>
+                        <li><a>About</a></li>
+                        <li><a>Search</a></li>
+                    </ul>
+                </div>
+                <a className="text-4xl font-bold">Recipe Calories</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1 text-xl font-medium">
+                    <li><a>Home</a></li>
+                    <li><a>Recipes</a></li>
+                    <li><a>About</a></li>
+                    <li><a>Search</a></li>
+                </ul>
+            </div>
+            <div className="navbar-end gap-5">
+                <label className="input input-bordered rounded-full hidden md:flex items-center gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+                    <input type="text" className="grow" placeholder="Search" />
+                </label>
+                <div className="bg-[#0BE58A] rounded-full p-1">
+                <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="circle-user" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="svg-inline--fa fa-circle-user fa-xl w-8"><path fill="currentColor" d="M406.5 399.6C387.4 352.9 341.5 320 288 320H224c-53.5 0-99.4 32.9-118.5 79.6C69.9 362.2 48 311.7 48 256C48 141.1 141.1 48 256 48s208 93.1 208 208c0 55.7-21.9 106.2-57.5 143.6zm-40.1 32.7C334.4 452.4 296.6 464 256 464s-78.4-11.6-110.5-31.7c7.3-36.7 39.7-64.3 78.5-64.3h64c38.8 0 71.2 27.6 78.5 64.3zM256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-272a40 40 0 1 1 0-80 40 40 0 1 1 0 80zm-88-40a88 88 0 1 0 176 0 88 88 0 1 0 -176 0z" className=""></path></svg>
+                </div>
+            </div>
+        </div>
+    )
+}
